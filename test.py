9bbec701d8a34ef76a73e955fca59701ea16c0e6
GuaccountIss <fedorchuk.egor@mail.ru>
2024-03-25 14:43:36 +0300

    задание 2 занятия

---
+++ test.py
@@ -0,0 +1,9 @@
+def check_password(password, confirmation):
+    if password == confirmation:
+        return "Пароль принят"
+    else:
+        return ""
+
+# Ввод пароля и подтверждения
+password = input("Введите пароль: ")
+confirmation = input("Подтвердите пароль: ")
